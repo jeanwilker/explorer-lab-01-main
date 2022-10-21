ef8c146086a1a998278f648fc60e3f2c42c9f94a
Jean Wilker <jeanwilkerss@gmail.com>
2022-10-21 00:59:39 -0300

    Cria função para data de expiração.

--- src/main.js
+++ src/main.js
@@ -72,3 +72,32 @@ function updateCardHolder() {
   const ccHolder = document.querySelector(".cc-holder .value")
   ccHolder.innerText = cardHolder.value === "" ? "FULANO DA SILVA" : cardHolder.value
 }
+
+//expiration date
+const expirationDate = document.querySelector("#expiration-date")
+const expirationDatePattern = {
+  mask: "MM{/}YY",
+  blocks: {
+    YY: {
+      mask: IMask.MaskedRange,
+      from: String(new Date().getFullYear()).slice(2),
+      to: String(new Date().getFullYear() + 10).slice(2),
+    },
+    MM: {
+      mask: IMask.MaskedRange,
+      from: 1,
+      to: 12,
+    },
+  },
+}
+
+const expirationDateMask = IMask(expirationDate, expirationDatePattern)
+
+expirationDateMask.on("accept", () => {
+  updateExpirationDat(expirationDateMask.value)
+})
+
+function updateExpirationDat(date) {
+  const ccExpiration = document.querySelector(".cc-expiration .value")
+  ccExpiration.innerText = date === "" ? "02/32" : date
+}
