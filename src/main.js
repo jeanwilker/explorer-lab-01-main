637b36640751bed413df1bdf9e388e4be688427b
Jean Wilker <jeanwilkerss@gmail.com>
2022-10-21 01:10:19 -0300

    Cria função para adicionar código CVC do cartão.

--- src/main.js
+++ src/main.js
@@ -101,3 +101,20 @@ function updateExpirationDat(date) {
   const ccExpiration = document.querySelector(".cc-expiration .value")
   ccExpiration.innerText = date === "" ? "02/32" : date
 }
+
+//securityCode
+const securityCode = document.querySelector("#security-code")
+const securityCodePattern = {
+  mask: "0000",
+}
+
+const securityCodeMasked = IMask(securityCode, securityCodePattern)
+
+securityCodeMasked.on("accept", () => {
+  updateSecurityCode(securityCodeMasked.value)
+})
+
+function updateSecurityCode(code) {
+  const ccSecurity = document.querySelector(".cc-security .value")
+  ccSecurity.innerText = code === "" ? "123" : code
+}
